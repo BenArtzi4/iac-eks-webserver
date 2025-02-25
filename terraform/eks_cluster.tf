9fddb28e0d9f4e06683bdd75fdaddf867c8b54f8
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2025-02-26 00:30:40 +0200

    change to private subnets and changed to vars instead hardcoded

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -2,20 +2,18 @@
 # EKS Cluster
 ##################################################
 
-# Declares a new Terraform resource 
 resource "aws_eks_cluster" "this" {
-  name     = "my-eks-cluster"
+  name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
-  # Defines the VPC networking configuration for the EKS cluster
+  # Use private subnets for EKS Cluster
   vpc_config {
     subnet_ids = [
-      aws_subnet.public_subnet_1.id,
-      aws_subnet.public_subnet_2.id
+      aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id
     ]
   }
 
-  # Ensures that Terraform creates and attaches the required IAM policies before creating the EKS cluster
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy,
     aws_iam_role_policy_attachment.eks_service_policy
