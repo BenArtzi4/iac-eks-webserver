787335db858bf4a6c954e794a8eb8e7c191c3182
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2025-02-26 00:31:23 +0200

    change to private subnets

--- terraform/node_group.tf
+++ terraform/node_group.tf
@@ -2,21 +2,20 @@
 # EKS Node Group (Worker Nodes)
 ##################################################
 
-# Create the EKS Node Group
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "my-eks-nodes"
-  node_role_arn   = aws_iam_role.node_group_role.arn  # Moved IAM role definition to iam_control_plane.tf
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.node_group_role.arn
 
   subnet_ids = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
   ]
 
   scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
+    desired_size = var.node_group_desired_size
+    min_size     = var.node_group_min_size
+    max_size     = var.node_group_max_size
   }
 
   depends_on = [
