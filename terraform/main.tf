9704aa9ec12ca3895fafcdf08f0ecc24059d720c
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2025-02-25 22:40:53 +0200

    remove data sources for default VPC and subnets, changed to vpc.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,10 @@
-# Create EKS Cluster
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+terraform {
+  required_version = ">= 1.0.0"
 
-  vpc_config {
-    subnet_ids = var.subnet_ids
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy,
-    aws_iam_role_policy_attachment.eks_service_policy
-  ]
-}
-
-# Create Node Group
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.cluster_name}-nodegroup"
-  node_role_arn   = aws_iam_role.eks_nodes_role.arn
-  subnet_ids      = var.subnet_ids
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
