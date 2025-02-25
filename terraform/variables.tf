2b64a417bcef5e9041f52048d980874474df4a3f
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2025-02-26 00:32:10 +0200

    update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,12 +31,12 @@ variable "public_subnet_2_cidr" {
   default = "10.0.2.0/24"
 }
 
-variable "private_subnet_1_cidr" {  # 🔹 Missing variable added
+variable "private_subnet_1_cidr" {
   type    = string
   default = "10.0.3.0/24"
 }
 
-variable "private_subnet_2_cidr" {  # 🔹 Missing variable added
+variable "private_subnet_2_cidr" {
   type    = string
   default = "10.0.4.0/24"
 }
@@ -50,22 +50,31 @@ variable "availability_zones" {
 # Variables for EKS Node Group
 ##################################################
 
-variable "node_group_name" {  # 🔹 Missing variable added
+variable "node_group_name" {
   type    = string
   default = "my-eks-nodes"
 }
 
-variable "node_group_min_size" {  # 🔹 Missing variable added
+variable "node_group_min_size" {
   type    = number
   default = 1
 }
 
-variable "node_group_desired_size" {  # 🔹 Missing variable added
+variable "node_group_desired_size" {
   type    = number
   default = 2
 }
 
-variable "node_group_max_size" {  # 🔹 Missing variable added
+variable "node_group_max_size" {
   type    = number
   default = 3
+}
+
+##################################################
+# Variables for Web Server Deployment
+##################################################
+
+variable "webserver_image" {  # 🔹 Added missing variable
+  type    = string
+  default = "123456789012.dkr.ecr.us-east-1.amazonaws.com/my-web-app:latest"  # Replace with your actual ECR repository
 }
